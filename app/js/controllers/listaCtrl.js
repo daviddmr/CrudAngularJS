12d214d8b536ac68ada3186f5592141d992c118c
David <david.melo@indexdigital.com.br>
2016-04-11 02:27:15 -0300

    Begin of change of the request to main backend

--- app/js/controllers/listaCtrl.js
+++ app/js/controllers/listaCtrl.js
@@ -4,9 +4,13 @@ angular.module("CrudAgro")
         $scope.users = [];
 
         var loadUsersListFromBackNode = function () {
-            $http.get("http://localhost:3412/users").success(function (data, status) {
+            //$http.get("http://localhost:3412/users").success(function (data, status) {
+            console.log("Tá tentando");
+            $http.get("http://localhost:8080/Restful/user/listarTodos").success(function (data, status) {
                 console.log(data);
                 $scope.users = data;
+            }).error(function (data, status) {
+                $scope.message = "Aconteceu um problema: "+ data;
             });
         };
 
