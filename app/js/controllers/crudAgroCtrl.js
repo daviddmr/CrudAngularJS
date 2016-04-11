6f7c37fb819f3ccbd405862f2bacaaa14f88f777
David <david.dmr@gmail.com>
2016-04-11 12:10:36 -0300

    Refatoring of crudAgroCtrl

--- app/js/controllers/crudAgroCtrl.js
+++ app/js/controllers/crudAgroCtrl.js
@@ -1,6 +1,8 @@
 angular.module("CrudAgro")
-    .controller("crudAgroCtrl", function ($scope){
+    .controller("crudAgroCtrl", ['$scope', '$rootScope', function ($scope, $rootScope){
         $scope.control = "Crud Agro Control";
         $scope.user = {};
 
-    });
+        $rootScope.action = "";
+
+    }]);
