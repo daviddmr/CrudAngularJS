22f4bd634d000a65c5eed18c03f542f3563c5439
David <david.dmr@gmail.com>
2016-04-12 20:12:46 -0300

    Set information when the page is for update ou create user/addUser implemented/updateUser doesnt work

--- app/js/controllers/cadastroCtrl.js
+++ app/js/controllers/cadastroCtrl.js
@@ -4,8 +4,7 @@ angular.module("CrudAgro")
 
         $scope.action = $routeParams.action;
 
-        var getUser = function(){
-            var id = 1;
+        var getUser = function(id){
             $http.get("http://localhost:8080/Restful/user/listarUm?id="+id).success(function (data, status) {
                 data.birthday = new Date(data.birthday);
                 $scope.user = data;
@@ -16,31 +15,58 @@ angular.module("CrudAgro")
 
         if($scope.action === 'Editar'){
             $scope.title = "Edição de usuário";
-            getUser();
+            $scope.titleOfButton = "Editar";
+            getUser($routeParams.id);
         }else{
             $scope.title = "Cadastro de novo usuário";
+            $scope.titleOfButton = "Salvar";
         }
 
-        $scope.addUser = function(user) {
-            console.log(user);
-            console.log("1 - Nome = "+ user.firstName+ " Sobrenome = " +user.lastName);
-            if(user.firstName == null){
-                user.firstName = "Armando";
+        $scope.addUpdateUser = function(user) {
+            if($scope.action === 'Editar'){
+                console.log("calling update post");
+                $http.post("http://localhost:8080/Restful/user/update",
+                    "id=" + encodeURIComponent(user.id) +
+                    "&first_name=" + encodeURIComponent(user.firstName) +
+                    "&last_name=" + encodeURIComponent(user.lastName) +
+                    "&birthday=" + encodeURIComponent(user.birthday) +
+                    "&address=" + encodeURIComponent(user.address) +
+                    "&address_complement=" + encodeURIComponent(user.addressComplement) +
+                    "&district=" + encodeURIComponent(user.district) +
+                    "&telephone=" + encodeURIComponent(user.telephone) +
+                    "&mobile_phone=" + encodeURIComponent(user.mobilePhone) +
+                    "&rg=" + encodeURIComponent(user.rg) +
+                    "&cpf=" + encodeURIComponent(user.cpf) +
+                    "&state=" + encodeURIComponent(user.state) +
+                    "&city=" + encodeURIComponent(user.city) +
+                    "&postcode=" + encodeURIComponent(user.postcode)
+                ).success(function (data, status) {
+                        console.log("Success Response = "+data+ " Status = "+status);
+                }).error(function (data, status){
+                    console.log("Error Response = "+data+ " Status = "+status);
+                });
+            }else{
+                console.log("calling insert post");
+                $http.post("http://localhost:8080/Restful/user/add",
+                    "first_name=" + encodeURIComponent(user.firstName) +
+                    "&last_name=" + encodeURIComponent(user.lastName) +
+                    "&birthday=" + encodeURIComponent(user.birthday) +
+                    "&address=" + encodeURIComponent(user.address) +
+                    "&address_complement=" + encodeURIComponent(user.addressComplement) +
+                    "&district=" + encodeURIComponent(user.district) +
+                    "&telephone=" + encodeURIComponent(user.telephone) +
+                    "&mobile_phone=" + encodeURIComponent(user.mobilePhone) +
+                    "&rg=" + encodeURIComponent(user.rg) +
+                    "&cpf=" + encodeURIComponent(user.cpf) +
+                    "&state=" + encodeURIComponent(user.state) +
+                    "&city=" + encodeURIComponent(user.city) +
+                    "&postcode=" + encodeURIComponent(user.postcode)
+                ).success(function (data, status) {
+                        console.log("Success Response = "+data+ " Status = "+status);
+                }).error(function (data, status){
+                    console.log("Error Response = "+data+ " Status = "+status);
+                });
             }
-            if(user.lastName == null){
-                user.lastName = "Teste";
-            }
-            console.log("2 - Nome = "+ user.firstName+ " Sobrenome = " +user.lastName);
-            $http.post("http://localhost:8080/Restful/user/add", user.firstName, user.lastName).success(function (data, status) {
-                delete $scope.user;
-                //loadUsersListFromBackNode();
-                console.log("Success Response = "+data+ " Status = "+status);
-                //$scope.user = {};
-                 //$scope.contatos.push(contato); //adiciona o contato que foi incluso na chamada do post
-                // $scope.contatos.push(data); //adiciona o contato que é o retorno da operação de post
-            }).error(function (data, status){
-                console.log("Error Response = "+data+ " Status = "+status);
-            });
         };
 
         $scope.clearForm = function(form) {
